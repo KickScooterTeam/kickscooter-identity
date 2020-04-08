a3314a34574ab07ea46eed2d0eafc4ba2c9a74ab
Edgar <edgarkrit@gmail.com>
2020-04-08 19:01:26 +0300

    added preauthorise for admin endpoints

--- identity-service/src/main/java/com/softserve/identityservice/controller/UserController.java
+++ identity-service/src/main/java/com/softserve/identityservice/controller/UserController.java
@@ -7,6 +7,7 @@ import com.softserve.identityservice.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -36,11 +37,13 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/admin/users/{userId}/block")
     public ResponseEntity<Long> blockUser(@PathVariable UUID userId) {
         return ResponseEntity.ok(userService.blockUser(userId));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{id}")
     public ResponseEntity<UserInfoResponse> userResponse(@PathVariable UUID id) {
         return ResponseEntity.ok(userService.userInfo(id));
